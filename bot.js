e686506c6546165aa0f79ff49178d352bf3124df
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-08 14:38:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,20 @@
-const Discord = require('discord.js');
+ const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+console.log("Scrpit By Dream");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
 
-client.on('ready', async() => {
-var server = "509798114484420609"; // ايدي السررفر
-var channel = "509799107309731840";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('شلونك انا')
-    },305);
+client.on("ready", () => {
+let channel =     client.channels.get("510052992968425493")
+setInterval(function() {
+channel.send(`! αgнιℓαѕ|ค๒๓`);
+}, 30)
 })
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
- 
-client.login(process.env.BOT_TOKEN);
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "say") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
